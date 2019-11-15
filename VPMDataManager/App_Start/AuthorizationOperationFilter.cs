8a21f4c4e5dfd6697843d2c4517a17c095435ba7
Leonel Villa <leovilla_91@hotmail.com>
2019-11-14 21:06:50 -0300

    Agregar y configurar Swagger

---
+++ VPMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,29 @@
+﻿using Swashbuckle.Swagger;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+
+namespace VPMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            // Asegurarse de que la operación tiene una lista de parametros
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access_token",
+                required = false,
+                type = "string",
+                @default = "bearer "
+            });
+
+        }
+    }
+}
