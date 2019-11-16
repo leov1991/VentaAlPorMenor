f0b1284885603c39738dfb55c4568045d597b5f5
Leonel Villa <leovilla_91@hotmail.com>
2019-11-16 00:27:05 -0300

    Consultar base de datos
    Creado proyecto de acceso a datos para la API usando Dapper y stored procedures.

---
+++ VPMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using VPMDataManager.Library.Internal.DataAccess;
+using VPMDataManager.Library.Models;
+
+namespace VPMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string id)
+        {
+            SQLDataAccess sql = new SQLDataAccess();
+
+            var p = new { Id = id };
+
+            var output = sql.LoadData<UserModel, dynamic>("[dbo].[spUserLookup]", p, "VPMData");
+
+            return output;
+        }
+    }
+}
---
+++ VPMDataManager/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+using VPMDataManager.Library.DataAccess;
+using VPMDataManager.Library.Models;
+
+namespace VPMDataManager.Controllers
+{
+
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+
+        public List<UserModel> GetById()
+        {
+            string id = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(id);
+
+        }
+
+    }
+}
